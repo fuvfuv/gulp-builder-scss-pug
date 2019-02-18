329a8ab60e69ba1ca1c3d18cea0af37fa2b079b2
uqrock <yury.beloborodov@gmail.com>
2019-02-18 10:30:02 +0300

    Изменил сборку стилей

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,8 +1,8 @@
 const plumber = require('gulp-plumber');
-const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
+const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
+const mincss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const gulpif = require('gulp-if');
 
@@ -16,8 +16,25 @@ module.exports = function () {
         browsers: ['last 3 versions'],
         cascade: false
       })))
-      .pipe(gulpif($.production, csso()))
       .pipe(sourcemaps.write())
+      .pipe(gulpif($.production, mincss({
+        compatibility: "ie8",
+        level: {
+          1: {
+            specialComments: 0,
+            removeEmpty: true,
+            removeWhitespace: true
+          },
+          2: {
+            mergeMedia: true,
+            removeEmpty: true,
+            removeDuplicateFontRules: true,
+            removeDuplicateMediaBlocks: true,
+            removeDuplicateRules: true,
+            removeUnusedAtRules: false
+          }
+        }
+      })))
       .pipe(rename('style.min.css'))
       .pipe($.gulp.dest('build/css/'))
       .pipe($.browserSync.reload({
